bd74f7647fdd9c79ba43b87ef9fe15b7fa225c42
BHamp <brianscotthampton@gmail.com>
2020-04-29 22:13:49 -0400

    Built basic server functionality

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const parser = require('body-parser');
+const port = 8000;
+
+app.get('/media', (req, res) => {
+  console.log('get request works!')
+  res.send('get request works!')
+})
+
+app.listen(port, () => {console.log(`listening at http://localhost:${port}`)});
