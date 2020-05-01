e42423ff0d1e4001693190788f126f0e013f247e
BHamp <brianscotthampton@gmail.com>
2020-05-01 16:49:41 -0400

    Will successfully seed data into db

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const app = express();
-const path = require('path');
 const parser = require('body-parser');
+const path = require('path');
+
+const app = express();
 const port = 8000;
 const db = require('../database/config.js')
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/media', (req, res) => {
-  console.log('made it to endpoint!')
+  // console.log('made it to endpoint!');
   const callback = (err, data) => {
     if(err) {
       res.status(404).json(err);
@@ -16,7 +17,7 @@ app.get('/media', (req, res) => {
       res.status(200).json(data);
     }
   };
-  db.getGame(req.body._id, callback)
+  db.getGame(req.body._id, callback);
 })
 
-app.listen(port, () => {console.log(`listening at http://localhost:${port}`)});
+app.listen(port, () => { console.log(`listening at http://localhost:${port}`); });
