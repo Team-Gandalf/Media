636544b5b082cbc70d3a6629cc6e7a6f43b28eaa
BHamp <brianscotthampton@gmail.com>
2020-05-04 15:51:47 -0400

    Basic font styling for title and summary

--- client/components/Summary.jsx
+++ client/components/Summary.jsx
@@ -11,18 +11,18 @@ class Summary extends React.Component {
   }
 
   render() {
-    {console.log(this.props.details, '!!!!!!!!!!!!!!!')}
     return (
-      <div>
-        <div><img src={this.props.details.splash} alt='Game Splash'></img></div>
-        <p>{this.props.details.description}</p>
-        <p>
-          ALL REVIEWS:  <span className='blerb'>{this.props.details.reviews.general}</span>
-           <span className='reviewCount'>{this.props.details.reviews.total}</span>
+      <div className='summary'>
+        <div className='splash'><img src={this.props.details.splash} alt='Game Splash'></img></div>
+        <p className='description'>{this.props.details.description}</p>
+        <p className='header'>
+          ALL REVIEWS:  <span className='reviewValue'>{this.props.details.reviews.general}
+           <span className='reviewCount'> ({this.props.details.reviews.total})</span></span>
         </p>
-        <p>RELEASE DATE: <span className ='releaseDate'>{this.props.details.releaseDate}</span></p>
-        <p>DEVELOPER: <span className='blerb'>{this.props.details.developer}</span></p>
-        <p>Publisher: <span className='blerb'>{this.props.details.publisher}</span></p>
+        <p className='header'>RELEASE DATE: <span className ='releaseDate'>{this.props.details.releaseDate}</span></p>
+        <p className='header'>DEVELOPER: <a className='blerb'>{this.props.details.developer}</a></p>
+        <p className='header'>PUBLISHER: <a className='blerb'>{this.props.details.publisher}</a></p>
+        <p className='tagHeader'>Popular user-defined tags for this product:</p>
         <Tags />
       </div>
     );
